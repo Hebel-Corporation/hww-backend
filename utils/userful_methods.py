a126cd1d8c5a2535424b721fea4f1327f72b0aee
bienfaitLuvako <bienfaitluvako@gmail.com>
2024-09-04 12:35:39 +0200

    refactored some code in IDs generation method

--- utils/userful_methods.py
+++ utils/userful_methods.py
@@ -13,6 +13,7 @@ def get_new_company_id(id_type:IdType,location=None,office=None,member=None):
     from authentication.models import CustomUser
 
     last_instance = None
+    new_instance_id = ''
     new_company_id =''
 
     if id_type==IdType.ACCOUNT:
@@ -21,26 +22,21 @@ def get_new_company_id(id_type:IdType,location=None,office=None,member=None):
         last_instance = Office.objects.filter(location=location).order_by('-created_at').first()
     else:
         last_instance = CustomUser.objects.filter(user_type="staff" if id_type==IdType.STAFF else "member").order_by('-date_joined').first()
-    
-    user_initial = "S" if id_type==IdType.STAFF else "M"  
+
 
     if last_instance:
-    
         last_instance_id = int(re.findall(r'[0-9]+',last_instance.company_id.split('-').pop()).pop())
-        
-        if id_type==IdType.ACCOUNT:
-            new_company_id = member.company_id+"-"+str(last_instance_id+1).zfill(2)
-        elif id_type == IdType.OFFICE:
-            new_company_id = location.code+str(last_instance_id+1).zfill(3)
-        else:
-            new_company_id = office.company_id+"-"+user_initial+str(last_instance_id+1).zfill(2 if id_type==IdType.STAFF else 5)
-    else:
+        new_instance_id = str(last_instance_id+1)
+    else :
+        new_instance_id = "1"
 
-        if id_type==IdType.ACCOUNT:
-            new_company_id = member.company_id+"-"+"1".zfill(2)
-        elif id_type == IdType.OFFICE:
-            new_company_id = location.code+"1".zfill(3)
-        else:
-            new_company_id = office.company_id+"-"+user_initial+"1".zfill(5)
+    user_initial = "S" if id_type==IdType.STAFF else "M"  
+        
+    if id_type==IdType.ACCOUNT:
+        new_company_id = member.company_id+"-"+new_instance_id.zfill(2)
+    elif id_type == IdType.OFFICE:
+        new_company_id = location.code+new_instance_id.zfill(3)
+    else:
+        new_company_id = office.company_id+"-"+user_initial+new_instance_id.zfill(2 if id_type==IdType.STAFF else 5)
 
     return new_company_id
