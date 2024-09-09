3aba2af8912fb5201e97f685dc589aa255a8fbad
bienfaitLuvako <bienfaitluvako@gmail.com>
2024-09-09 16:49:00 +0200

    corrected authentication issue

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 @admin.register(CustomUser)
-# class CustomUserAdmin(UserAdmin,admin.ModelAdmin):
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin,admin.ModelAdmin):
+# class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'company_id','user_type', 'username', 'first_name', 'last_name', 'is_active']
     # fieldsets = ['id', 'username', 'first_name', 'last_name', 'is_active', 'is_office_admin']
