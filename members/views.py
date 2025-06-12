2898933c428f63e66eae2d295f552ac8064defdf
Nelson Kayisirirya <nelson.kayisirirya@gmail.com>
2025-06-12 20:15:21 +0100

    Finalization of company sold logics.

--- members/views.py
+++ members/views.py
@@ -32,6 +32,7 @@ import calendar
 import json
 from django.utils import timezone
 from datetime import timedelta
+from itertools import chain
 
 
 class CountryViewSet(viewsets.ModelViewSet) :
@@ -200,7 +201,19 @@ class OfficeViewSet(viewsets.ModelViewSet) :
         
         if activity_type == 'TOTALS':
 
-            subscriptions = Subscription.objects.all()
+            referral_ids = referrals.values_list('downline', flat=True)
+            purchase_bonus_ids = purchase_bonus.values_list('sale_detail__member_account', flat=True)
+            recent_matching_ids = [
+                mt.downlines.order_by('-created_at').first().id
+                for mt in matchings.prefetch_related('downlines')
+                if mt.downlines.exists()
+            ]
+
+            all_bonus_ids = list(chain(referral_ids, purchase_bonus_ids, recent_matching_ids))
+
+            subscriptions = Subscription.objects.filter(
+                member_account__in=all_bonus_ids
+            )
 
             if office_id and office_id != 'all' :
                 subscriptions = subscriptions.filter(office__id=office_id)
@@ -215,12 +228,12 @@ class OfficeViewSet(viewsets.ModelViewSet) :
             elif period_filter == 'monthly' :
                 subscriptions = subscriptions.filter(created_at__year=now.year, created_at__month=now.month)
 
-            total_matchings = matchings.aggregate(total=Sum('amount'))['total'] or 0
-            total_referrals = referrals.aggregate(total=Sum('amount'))['total'] or 0
-            total_purchase_bonus = purchase_bonus.aggregate(total=Sum('amount_to_be_paid'))['total'] or 0
+            total_matchings = matchings.aggregate(total=Sum('amount'))['total'] if matchings else 0
+            total_referrals = referrals.aggregate(total=Sum('amount'))['total'] if referrals else 0
+            total_purchase_bonus = purchase_bonus.aggregate(total=Sum('amount_to_be_paid'))['total'] if purchase_bonus else 0
 
+            total_received = subscriptions.aggregate(total=Sum('package__price'))['total'] or 0 + (purchase_bonus.aggregate(total=Sum('sale_detail__amount'))['total'] if purchase_bonus else 0)
             total_bonuses = total_matchings + total_referrals + total_purchase_bonus
-            total_received = subscriptions.aggregate(total=Sum('package__price'))['total'] or 0 + purchase_bonus.aggregate(total=Sum('sale_detail__amount'))['total'] or 0
 
 
             activity_data = {
